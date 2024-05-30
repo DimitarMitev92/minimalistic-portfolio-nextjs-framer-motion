e46f000980b0d6ef8ff6c31c8aeca988c3f52d06
Dimitar Mitev <d.mitev920123@gmail.com>
2024-05-30 09:54:08 +0300

    feat(): final commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+
 import "./globals.css";
 import { ThemeProvider } from "./provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Dimitar's portfolio",
-  description: "Modern & Minimalist Portfolio",
+  title: "Dimitar's Portfolio",
+  description: "Modern & Minimalistic Portfolio",
 };
 
 export default function RootLayout({
@@ -16,7 +17,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/jsm-logo.png" sizes="any" />
+      </head>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
